2d0da660c771eabd4be2ab1c0e8f36366cbbf25c
Brice Djilo <brice.djilo@endurance.com>
2020-10-25 13:08:13 -0600

    banner

--- src/components/banner/banner.jsx
+++ src/components/banner/banner.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
+import { NavHashLink } from "react-router-hash-link";
+
 import Carousel from "react-bootstrap/Carousel";
 
 import banner from "./images/banner-rc.jpg";
-// import bannerDepistage from "./images/banner-depistage.jpeg";
+import bannerDepistage from "./images/banner-depistage-2.png";
 
 import "./banner.css";
 
@@ -48,60 +50,15 @@ export const Banner = (props) => {
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item key="02">
-        <img className="d-block img-fluid w-100" src={banner} alt="2nd slide" />
-        <div className="banner-content slogan w-100">
-          <h1>
-            Votre <span className="theme-color font-l"> Santé</span>
-            ,
-            <br />
-            Notre
-            <span className="primary-color font-l"> Satisfaction </span>
-          </h1>
-          <a href="#footer" className="btn btn-success btn-sm btn-rdv">
-            Obtenez un rendez-vous{" "}
-            <i className="icofont-rounded-double-right"></i>
-          </a>
-        </div>
-        <Carousel.Caption>
-          <p
-            style={{
-              color: "#00aa01",
-              "font-size": "2em",
-              "font-weight": "bold",
-            }}
-            className="banner-caption"
-          >
-            Soins et services de qualité dans un cadre royal
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item key="03">
-        <img className="d-block img-fluid w-100" src={banner} alt="3rd slide" />
-        <div className="banner-content slogan w-100">
-          <h1>
-            Votre <span className="theme-color font-l"> Santé</span>
-            ,
-            <br />
-            Notre
-            <span className="primary-color font-l"> Satisfaction </span>
-          </h1>
-          <a href="#footer" className="btn btn-success btn-sm btn-rdv">
-            Obtenez un rendez-vous{" "}
-            <i className="icofont-rounded-double-right"></i>
-          </a>
-        </div>
-        <Carousel.Caption>
-          <p
-            style={{
-              color: "#00aa01",
-              "font-size": "2em",
-              "font-weight": "bold",
-            }}
-            className="banner-caption"
-          >
-            Soins et services de qualité dans un cadre royal
-          </p>
-        </Carousel.Caption>
+        <NavHashLink to="#depistages">
+          <img
+            className="d-block img-fluid w-100"
+            src={bannerDepistage}
+            alt="2nd slide"
+            style={{ float: "right" }}
+          />
+        </NavHashLink>
+        <Carousel.Caption></Carousel.Caption>
       </Carousel.Item>
     </Carousel>
   );
